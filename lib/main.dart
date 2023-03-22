aa924f7dc72b44c988f4706712c3b3d7920d8feb
muhammad yunus <turmudi.yunus@gmail.com>
2023-03-22 21:57:08 +0700

    remove debug label

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // Add the line below to get horizontal sliding transitions for routes.
         pageTransitionsTheme: const PageTransitionsTheme(builders: {
